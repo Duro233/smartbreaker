4de6224f550da73886f36a8ac8d64739421a21c4
Duro233 <146140907+Duro233@users.noreply.github.com>
2026-02-20 17:14:17 -0500

    Update HomePage layout and integrate NavbarSimple
    Refactor HomePage component layout and add NavbarSimple.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,18 +1,22 @@
 import getUser from '../routes/getuser';
+import { NavbarSimple } from '../components/NavbarSimple';
 //import '../styles/theme.css';
 
 export default function HomePage()
 {
   const userInfo = getUser();
   console.log(userInfo);
+  
   return (
-    <div style={{background: 'var(--app-bg)'}}>
-      <h2>Home Page</h2>
+    <div style={{ display: 'flex', height: '100vh', background: 'var(--app-bg)' }}>
+      <NavbarSimple />
+      <main style={{ flex: 1, padding: '2rem'}}>
         <h2>{userInfo?.first} {userInfo?.last}</h2>
         <h3>Smartbreaker Details</h3>
-        //details on the smartbreaker
+        {/*details on the smartbreaker*/}
         <h4>Set Up Instructions</h4>
-        //info on how to setup the smartbreaker
+        {/*info on how to setup the smartbreaker*/}
+      </main>
     </div>
   );
 } 
